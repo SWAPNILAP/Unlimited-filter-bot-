8f20ed37acb23b8c41620ae65b6ae60aeeac3242
SWAPNILAP <132188201+SWAPNILAP@users.noreply.github.com>
2023-05-08 12:52:34 +0530

    Update script.py

--- script.py
+++ script.py
@@ -34,13 +34,13 @@ Add me as admin in your group and start filtering :)
 /id  -  Shows ID information
 /info userid  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
-© @Mo_Tech_YT
+© @Lion_098765
 """
 
 AboutTxT = """
 ⭕️ My Name : TroJanz Filter Bot</b>
-⭕️ Developers : @TroJanzHEX 
-⭕️ Upgrader : @Mo_Tech_YT
+⭕️ Developers : <a href='https://t.me/Lion_098765'4>Chekuthan</a>
+⭕️ Upgrader : @Lion_098765
 ⭕️ Language : Python3
 ⭕️ Library : <a href='https://docs.pyrogram.org/'>Pyrogram 2.0.102</a> 
 """
